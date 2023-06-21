f1af1fb2d53844c29ece01a76b4dc570e5f4d588
ThePythonGuyy <thepythonguy@outlook.com>
2023-06-22 01:29:20 +0530

    Navigate

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Navigate, redirect } from 'react-router-dom'
+// useState
 function About() {
+  const [value, setValue] = useState(true)
+
+  if(!value){
+    return <Navigate to='/' replace={true} />
+    //  redirect('/')
+  }
   return (
+
     <div>
         <h1>INTEL</h1>
         <p>Ten-year-old Ben Tennyson discovers a mysterious device, the Omnitrix, 
         on a family vacation. <br/>The device allows him to transform into ten 
         different alien forms replete with unique superpowers.
         </p>
+        <button onClick={() => setValue(false)}>Logout</button>
     </div>
   )
 }
